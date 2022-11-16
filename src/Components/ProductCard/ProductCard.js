489afeb458044a4349079fcdc4e72ae865430cd8
Rafi Kadir <rafikadir2000@gmail.com>
2022-11-16 20:06:19 +0600

    add order page

---
+++ src/Components/ProductCard/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProductCard.scss';
+
+const ProductCard = (product) => {
+    const {image,title} = product.product;
+
+    return (
+        <div className="product_table">
+           
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import PageTitle from '../../Components/PageTitle/PageTitle';
+import ProductCard from '../../Components/ProductCard/ProductCard';
 
 const Products = () => {
+
+    const [products, setProducts] = useState();
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
+    console.log(products);
+
     return (
-        <Container fluid>
-            <h1>Product page</h1>
+        <Container className="p-3" fluid>
+            <PageTitle title="Products"/>
+
+            <Row>
+                {
+                    products.map((product) =>
+                        <Col lg={3}>
+                            <ProductCard product={product}/>
+                        </Col>)
+                }
+            </Row>
         </Container>
     );
 };
