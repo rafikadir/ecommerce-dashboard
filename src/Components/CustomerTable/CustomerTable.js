2617435937956b43d62556215bab867d6bea0607
rafikadir <rafikadir2000@gmail.com>
2022-11-19 08:12:29 +0600

    coustomer updates

--- src/Components/CustomerTable/CustomerTable.js
+++ src/Components/CustomerTable/CustomerTable.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Button, Modal, Table } from 'react-bootstrap';
 import './CustomerTable.scss';
 import { customersInfo } from '../../Data/CustomerData';
+import { AlertTriangle, Edit2, Eye, Trash } from 'react-feather';
+import { useState } from 'react';
 
 const CustomerTable = () => {
-  
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () =>{
+        setShow(true)
+    };
+
     return (
-        <div className="customer_table">
+        <>        
+            <Modal show={show} onHide={handleClose} className="delete_alert">
+                <Modal.Header closeButton>
+                    <Modal.Title>Are you sure?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AlertTriangle/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cencel
+                    </Button>
+                    <Button variant="danger" onClick={handleClose}>
+                        Yes ! Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <div className="customer_table">
             <Table>
                 <thead>
                     <tr>
@@ -28,11 +55,17 @@ const CustomerTable = () => {
                             <td>{customer.password}</td>
                             <td>{customer.phone}</td>
                             <td>{customer.registered}</td>
+                            <td>
+                                <button className='action_btn'><Eye/></button>
+                                <button className='action_btn'><Edit2/></button>
+                                <button className='action_btn' onClick={handleShow}><Trash/></button>
+                            </td>
                         </tr>)
                     }
                 </tbody>
             </Table>
         </div>
+        </>
     );
 };
 
