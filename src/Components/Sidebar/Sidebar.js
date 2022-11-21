baa33d877d3e0fc178a3763ac0e4546d30096674
rafikadir <rafikadir2000@gmail.com>
2022-11-21 20:53:25 +0600

    sidebar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -3,12 +3,17 @@ import './Sidebar.scss';
 import logo from './../../Assets/Img/logo.svg';
 import nav from '../../Nav';
 import { Link } from 'react-router-dom';
+import { ChevronLeft } from 'react-feather';
 
 const Sidebar = ({open}) => {
     return (    
         <div className={open? 'sidebar_area' : 'sidebar_area close'}>
             <div className="sidebar_logo">
                 <img src={logo} alt="logo" />
+
+                <div className="close_icon">
+                    <ChevronLeft/>  
+                </div>
             </div>
 
             <div className="sidebar_menu">
